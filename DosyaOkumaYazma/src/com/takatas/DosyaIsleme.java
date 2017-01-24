39f639ff41ab4a06089363be40de57649421de40
takatas <soner@soner-pc>
2017-01-24 23:00:34 +0200

    commit2

--- DosyaOkumaYazma/src/com/takatas/DosyaIsleme.java
+++ DosyaOkumaYazma/src/com/takatas/DosyaIsleme.java
@@ -29,7 +29,7 @@ public class DosyaIsleme {
 		System.out.println("Deðer Giriniz...");
 		int deger = scanner.nextInt();
 		System.out.println("---------------------------------------");
-
+		System.out.println("---------------------------------------");
 
 		switch (deger) {
 		case 1:
